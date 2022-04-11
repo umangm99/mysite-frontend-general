d26d29ad4d5953caeaef3a5ae500937e205eac94
Umang Mittal <umangm99@gmail.com>
2022-04-12 09:59:25 +1000

    React prod build issues

--- ui.frontendReact/src/index.js
+++ ui.frontendReact/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const reactComponentForResourceType = {
-	'cba/components/content/comp1': require('./components/Comp1/Comp1').default,
+	'mysite/components/content/headingdescription': require('./components/HeadingDescription/HeadingDescription').default,
 	'cba/components/content/comp2': require('./components/Comp2/Comp2').default
 };
 
